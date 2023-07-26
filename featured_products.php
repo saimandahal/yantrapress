f020a08bdda1a48f92a816d8f7f82f1214a42e24
Mac <mac@Macs-MacBook-Pro.local>
2023-07-26 16:09:56 +0545

    template

---
+++ featured_products.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Featured Product Woocommerce
+Plugin URI:   https://www.yantracore.com 
+Description:  Plugin to create a custom settings for the woocommerce featured products. 
+Version:      1.0
+Author:       Yantracore 
+Author URI:   https://www.yantracore.com
+License:      GPL2
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/
+?>
